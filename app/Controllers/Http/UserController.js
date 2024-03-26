29628f85355fde4cfabde465c82f35f0ea2e937a
aldimiroalfredo <aldimiroalfredo@gmail.com>
2024-03-26 17:54:29 +0100

    this commit create userFreelancer

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -1,35 +1,64 @@
 'use strict'
 
-// const TypeUsers = use('App/Models/TypeUser')
-// const UserFree = use ('App/Models/UserFreelancer')
-// const UserComp = use ('App/Models/UserCompany')
+const UserFree = use('App/Models/UserFreelancer')
+const UserComp = use('App/Models/UserCompany')
 const User = use('App/Models/User')
+const Database = use('Database');
 
 class UserController {
 
     async store({ request, response }) {
 
-        const userData = request.only(["fullname", "binumber", "phone", "username", "email", "password", "id_gender", "id_city", "id_type_user"])
+        try {
 
-        console.log(userData)
+            const userData = request.only(
+                [
+                    "fullname",
+                    "binumber",
+                    "phone",
+                    "username",
+                    "email",
+                    "password",
+                    "id_gender",
+                    "id_city",
+                    "id_type_user",
+                    "id_speciality",
+                    "name_company",
+                    "nif"
+                ])
 
-        //  const type_use = await TypeUsers.findByOrFail('slug',slug)
-        const user = await User.create(userData)
-        //  const user = await type_use.users().create(userData)
+            let id_speciality = userData.id_speciality || 1
+            let name_company = userData.name_company
+            let nif = userData.nif
 
-        // if (slug === 'Freelancer') {
-        //     await UserFree.create({ id_user: user.id,id_speciality});
-        //   } else if (slug === 'Empresa') {
-        //     await UserComp.create({ usuario_id: user.id,name_company,nif});
-        //   }
+            const typeUser = await Database.from('type_users').where('id', userData.id_type_user).first()
 
-        return user
-        //     return response.created({
-        //         status:true,
-        //         data:user
-        //     })
-        // }
+            delete userData.id_speciality
+            delete userData.name_company
+            delete userData.nif
+
+            const user_email = await Database.select('*').from('users').where('email', userData.email).first()
+
+            if (user_email) {
+
+                return response.created({ message: 'Já existe um usuário com esse email', code: 201 })
+            }
+
+            const user = await User.create(userData)
+
+            if (typeUser.slug === 'Freelancer') {
+                await UserFree.create({ id_user: user.id, id_speciality: id_speciality });
+            } else if (typeUser.slug === 'Empresa') {
+                await UserComp.create({ id_user: user.id, name_company: name_company, nif: nif });
+            }
+
+            return response.created({ message: 'Usuário registado com sucesso', code: 200, data: user })
+
+        } catch (error) {
+            console.log(error)
+        }
     }
+
 }
 
 module.exports = UserController
